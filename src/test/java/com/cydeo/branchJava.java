a75596af9bc603ca5852b02214817b597929215c
MikeJ77 <mikejohnson3505@gmail.com>
2021-12-28 11:30:17 +0300

    second branch work

--- src/test/java/com/cydeo/branchJava.java
+++ src/test/java/com/cydeo/branchJava.java
@@ -3,5 +3,7 @@ package com.cydeo;
 public class branchJava {
     public static void main(String[] args) {
         System.out.println("first branch work");
+        System.out.println("second branch work");
+
     }
 }
