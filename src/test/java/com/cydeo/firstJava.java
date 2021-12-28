8cd5108aacd9a62c38a229c19339aa6b8379b071
MikeJ77 <mikejohnson3505@gmail.com>
2021-12-28 11:23:48 +0300

    second commit

--- src/test/java/com/cydeo/firstJava.java
+++ src/test/java/com/cydeo/firstJava.java
@@ -3,5 +3,7 @@ package com.cydeo;
 public class firstJava {
     public static void main(String[] args) {
         System.out.println("First Project");
+        System.out.println("Second Project");
+
     }
 }
